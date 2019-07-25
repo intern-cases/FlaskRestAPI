ec36c311f19124459a07d2adcf39162dc4d1d6c2
Ömer Faruk Aslandoğdu <omerfarukaslandogdu@Omers-MacBook-Air.local>
2019-07-25 14:07:26 +0300

    v1.1.1 adding admin restrictions on comment routes and setting parent id on comment model

---
+++ migrations/versions/06be8966a173_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 06be8966a173
+Revises: 06a39b83e3f6, d7e6165b029a
+Create Date: 2019-07-25 13:55:17.602159
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '06be8966a173'
+down_revision = ('06a39b83e3f6', 'd7e6165b029a')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
