bac76a302b4266ec50d1bd5f619941886c7e6acf
omerfarukaslandogdu <Omer1aslan?>
2019-08-05 13:23:40 +0300

    manager added

--- api/users.py
+++ api/users.py
@@ -1,34 +1,30 @@
 from flask import request, jsonify, abort, Blueprint
-from models.users import UserPointModel, UserRolesModel, UserModel, RoleModel
-from utils.extensions import db
-from schemas.users import UserPointSchema, UserSchema
+from models.users import UserPointModel, UserRolesModel
+from schemas.users import UserPointSchema
 from api.authentication import user_verifying, login_required, is_admin
+from manager.users import *
 
-user_schema = UserSchema()
-users_schema = UserSchema(many=True)
 user_points_schema = UserPointSchema(many=True)
 
 blueprint_users = Blueprint("users", __name__, url_prefix='/users/')
-@blueprint_users.route("/add_roles", methods=["POST"])
+""""@blueprint_users.route("/add_roles", methods=["POST"])
 def add_roles():
     role_name = request.json["role_name"]
     new_role = RoleModel(role_name)
-    db.session.add(new_role)
-    db.session.commit()
-    return jsonify("Role added.")
+    db_add(new_role)
+    db_commit()
+    return jsonify("Role added.")"""
 
 
 @blueprint_users.route("/set_roles", methods=["PUT"])
 @login_required
 def set_admin():
-    user = UserRolesModel.query.filter(
-        user_verifying == UserRolesModel.user_id).get(UserRolesModel.role_id)
-    if user.role_id == 2:
+    if is_admin(user_verifying()):
         user_id = request.json["user_id"]
         role_id = request.json["role_id"]
-        user = UserRolesModel.query.filter(user_id == UserRolesModel.user_id).first()
+        user = get_user_by_user_id(user_id)
         user.role_id = role_id
-        db.session.commit()
+        db_commit()
     else:
         return jsonify("You're not allowed to do this")
 
@@ -43,14 +39,12 @@ def add_user():
 
     if username is None or password is None or email is None:
         abort(404)
-    if UserModel.query.filter(username == UserModel.username or email == UserModel.email).first() is not None:
-        abort(404)
 
-    db.session.add(new_user)
-    db.session.commit()
+    db_add(new_user)
+    db_commit()
     new_user_role = UserRolesModel(new_user.user_id, role_id)
-    db.session.add(new_user_role)
-    db.session.commit()
+    db_add(new_user_role)
+    db_commit()
 
     return jsonify("Request done.")
 
@@ -58,7 +52,7 @@ def add_user():
 @blueprint_users.route("/set_points/<string:username>", methods=["POST"])
 @login_required
 def point_to_user(username):
-    user = UserModel.query.filter(username == UserModel.username).first()
+    user = get_user_by_username(username)
     if user_verifying() == user.user_id:
         return jsonify("You can't set points to yourself.")
     else:
@@ -66,8 +60,8 @@ def point_to_user(username):
         if 0 <= int(points) <= 10:
             user_id = user.user_id
             point_user = UserPointModel(user_id, int(points))
-            db.session.add(point_user)
-            db.session.commit()
+            db_add(point_user)
+            db_commit()
             return jsonify(point_user)
         else:
             return jsonify("You have to set points between 0 and 10.")
@@ -76,28 +70,28 @@ def point_to_user(username):
 @blueprint_users.route("/user_panel", methods=["GET"])
 @login_required
 def get_logged_user():
-    spesific_user = UserModel.query.filter(user_verifying() == UserModel.user_id).first()
+    spesific_user = get_user_by_user_id(user_verifying())
     result = user_schema.dump(spesific_user)
     return jsonify(result.data)
 
 
 @blueprint_users.route("/list", methods=["GET"])
 def get_users():
-    all_users = UserModel.query.all()
+    all_users = get_all_user()
     result = users_schema.dump(all_users)
     return jsonify(result.data)
 
 
 @blueprint_users.route("/<int:user_id>", methods=["GET"])
 def user_detail(user_id):
-    user = UserModel.query.filter(user_id == UserModel.user_id).first()
+    user = get_user_by_user_id(user_id)
     result = user_schema.dump(user)
     return jsonify(result.data)
 
 
 @blueprint_users.route("/<string:username>", methods=["GET"])
 def user_detail_by_username(username):
-    user = UserModel.query.filter(username == UserModel.username).first()
+    user = get_user_by_username(username)
     result = user_schema.dump(user)
     return jsonify(result.data)
 
@@ -105,7 +99,7 @@ def user_detail_by_username(username):
 @blueprint_users.route("/<string:username>", methods=["PUT"])
 @login_required
 def user_update(username):
-    user = UserModel.query.filter(username == UserModel.username).first()
+    user = get_user_by_username(username)
     if user.user_id == user_verifying() or is_admin(user_verifying()):
         username = request.json['username']
         password = request.json['password']
@@ -114,7 +108,7 @@ def user_update(username):
         user.username = username
         user.password_hash = password
 
-        db.session.commit()
+        db_commit()
         return jsonify(user)
     else:
         return jsonify("You're not allowed to do this action.")
@@ -124,9 +118,9 @@ def user_update(username):
 @login_required
 def user_delete(user_id):
     if user_id == user_verifying() or is_admin(user_verifying()):
-        user = UserModel.query.filter(user_verifying() == UserModel.user_id).first()
-        db.session.delete(user)
-        db.session.commit()
+        user = get_user_by_user_id(user_verifying())
+        db_delete(user)
+        db_commit()
         return jsonify(user)
     else:
         return jsonify("You're not allowed to do this action.")
@@ -135,10 +129,10 @@ def user_delete(user_id):
 @blueprint_users.route("/delete/<string:username>", methods=["DELETE"])
 @login_required
 def user_delete_by_username(username):
-    user = UserModel.query.filter(username == UserModel.username).first()
+    user = get_user_by_username(username)
     if user_verifying() == user.user_id or is_admin(user_verifying()):
-        db.session.delete(user)
-        db.session.commit()
+        db_delete(user)
+        db_commit()
         return jsonify(user)
     else:
         return jsonify("You're not allowed to do this action.")
